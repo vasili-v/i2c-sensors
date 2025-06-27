5ab3f6ba35154398e9e75c3273bb318f3b186fe2
Vasili Vasilyeu <vasili_vasilyeu@epam.com>
2025-06-27 00:09:35 -0700

    Implement CRC check for ENS210

--- src/i2cs/ens210.py
+++ src/i2cs/ens210.py
@@ -44,6 +44,37 @@ def _b_to_u(block):
 def _split(data):
     return _b_to_u(data[:2]), bool(data[2] & 0x1), (data[2] >> 1) & 0x7f
 
+_DATA7_WIDTH=17
+_DATA7_MASK=(1<<_DATA7_WIDTH)-1
+_DATA7_MSB=1<<_DATA7_WIDTH-1
+
+_CRC7_WIDTH=7
+_CRC7_POLY=0x89
+_CRC7_IVEC=0x7f
+
+_DATA7_MASK_HIGH=_DATA7_MASK<<_CRC7_WIDTH
+_CRC7_IPOL=_CRC7_POLY<<_DATA7_WIDTH-1
+_CRC7_IBIT=_DATA7_MSB<<_CRC7_WIDTH
+
+def _crc7(data):
+    x = ((_b_to_u(data) & _DATA7_MASK) << _CRC7_WIDTH) | _CRC7_IVEC
+    p = _CRC7_IPOL
+    b = _CRC7_IBIT
+
+    while b & _DATA7_MASK_HIGH:
+        if b & x:
+            x ^= p
+        p >>= 1
+        b >>= 1
+
+    return x
+
+def _repr_crc7(crc, crc_cmp):
+    if crc == crc_cmp:
+        return f'CRC: 0x{crc:02x} - ok'
+
+    return f'CRC: 0x{crc:02x}, 0x{crc_cmp:02x} - fail'
+
 class ENS210(Device):
     """ The class provides access to the basic functionality of ENS210 through I2C bus """
     def __init__(self, bus):
@@ -78,15 +109,21 @@ class ENS210(Device):
             self.write(_REG_SYS_CTRL, _REG_SYS_CTRL_LOW_POWER)
 
     def measure(self):
+        """ Measure temperature and relative humidity in one shot """
         start = time.monotonic_ns()
         self.write(_REG_SENS_START, _REG_SENS_START_H_START | _REG_SENS_START_T_START)
         time.sleep(_T_CONV_TH_SINGLE_SHORT)
 
         data = self.read(_REG_VAL)
+
         t_raw, t_valid, t_crc = _split(data[:3])
+        t_crc_str = _repr_crc7(t_crc, _crc7(data[:3]))
+
         h_raw, h_valid, h_crc = _split(data[3:])
+        h_crc_str = _repr_crc7(h_crc, _crc7(data[3:]))
+
         print(f'RH ({(time.monotonic_ns() - start)/1e6:.2f} ms)\n' \
-              f'\tT: 0x{t_raw:02x} (Valid: {t_valid}, CRC: 0x{t_crc:02x})\n' \
-              f'\tH: 0x{h_raw:02x} (Valid: {h_valid}, CRC: 0x{h_crc:02x})')
+              f'\tT: 0x{t_raw:02x} (Valid: {t_valid}, {t_crc_str})\n' \
+              f'\tH: 0x{h_raw:02x} (Valid: {h_valid}, {h_crc_str})')
 
         return t_raw/64 - 273.15, h_raw/512
